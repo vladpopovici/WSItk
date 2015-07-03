07fbceff5ec39fc8c6c013cc4121e41f77ec9135
vladpopovici <popovici@iba.muni.cz>
2015-07-03 10:41:57 +0200

    gap stat

--- WSItk/ml/gap.py
+++ WSItk/ml/gap.py
@@ -1,49 +1,81 @@
-# https://www.mail-archive.com/scikit-learn-general@lists.sourceforge.net/msg12635.html
+## This code was adapted from the code at
+## https://www.mail-archive.com/scikit-learn-general@lists.sourceforge.net/msg12635.html
+
 from __future__ import division
+
 import numpy as np
 from collections import Counter
 from sklearn.cluster import MiniBatchKMeans
 
-def gap(X):
-    '''
-    See http://web.stanford.edu/~hastie/Papers/gap.pdf
-    X has shape (n_samples, n_features) 
-    '''
+def gap(X, Ks, Wstar=None, B=0):
+    """
+    Gap statistic for estimating the "optimal" number of clusters.
+    For details see [1]_.
+    
+    :param X: numpy.ndarray
+        Input data, as a matrix with samples by rows.
+    :param Ks: enumerable
+        A list or numpy.vector or different numbers of clusters
+        to be tried.
+    :param Wstar: numpy.array
+        Estimates of the distribution of W* under a uniform random data
+        distribution. Can be reused in case of repeated estimations of K
+        for different (but similar) data sets X. If None, a new W* will
+        be generated.
+    :param B: int
+        Number of random data sets to be used in estimating
+        the null distribution. If Wstar is not None, B is obtained as
+        the number of rows of Wstar.
+    :return: a tuple
+        Two elements are returned:
+        -the estimated "optimal" K
+        -the matrix Wstar (see above)
+        
+    References:
+    -----------
+    .. [1] Tibshirani, Walther, Hastie. Estimating the number of clusters
+       in a data set via the gap statistic. J. R. Statist. Soc. B (2001) 63,
+       Part 2, pp 411-423. See: http://web.stanford.edu/~hastie/Papers/gap.pdf
+    """
+
     #Get the bounding box of the population
+
     mins, maxes = np.min(X, axis = 0), np.max(X, axis = 0)
-    B = 10        # number of reference datasets (from uniform distribution)
-    K = 9         # maximum number of clusters
-    W = np.zeros(K)                      # dispersion of im with k+1 clusters
-    Wstar = np.zeros([B, K])         # dispersion of dataset b with k+1 clusters
-    mbk = MiniBatchKMeans()
-    for k in range(K):
+    Ks = np.array(Ks)
+    Ks.sort()                                    # make sure they are sorted incresingly
+    nk = Ks.size
+    
+    K_max = Ks.max()                             # maximum number of clusters
+    
+    mbk = MiniBatchKMeans(compute_labels=True)
+    
+    if Wstar is None:
+        # generate the null distribution
+        Wstar = np.zeros([B, nk])                # dispersion of random dataset b with k+1 clusters
+        
+        for b in range(B):
+            dataset = np.random.rand(*X.shape) * (maxes-mins) + mins
+            for i, k in enumerate(Ks):
+                mbk.n_clusters = k+1
+                mbk.fit(dataset)
+                Wstar[b, i] = np.log(mbk.inertia_)
+
+    W = np.zeros(nk)                         # dispersion
+    
+    for i, k in enumerate(Ks):
         mbk.n_clusters = k+1 
         mbk.fit(X)
-        W[k] = np.log(mbk.inertia_)
-    
-    for b in range(B):
-        dataset = np.random.rand(*X.shape) * (maxes-mins) + mins
-        for k in range(K):
-            mbk.n_clusters = k+1
-            mbk.fit(dataset)
-            Wstar[b, k] = np.log(mbk.inertia_)
+        W[i] = np.log(mbk.inertia_)
             
     Gap = Wstar.mean(axis=0) - W
-    s = np.std(Wstar, axis=0) / np.sqrt(1+1/B)
-    try:
-        khat = 1+ min( [k for k in range (K-1) if Gap[k] >= Gap[k+1] - s[k+1]])
-    except ValueError:
-        khat = None
-    z = np.array([0])
-    Gap = np.hstack((z, Gap))
-    s = np.hstack((z, s))
-    return khat, Gap, s
+    s = np.std(Wstar, axis=0) / np.sqrt(1.0 + 1.0/B)
+    kl = [k for i, k in enumerate(Ks[:-1]) if Gap[i] >= Gap[i+1] - s[i+1]]
+    if len(kl) > 0:
+        # at least one reasonable k:
+        khat = min(kl)
+    else:
+        # just resturn the max:
+        khat = Ks[-1]
+    
+    return khat, Wstar
 
-if __name__ == '__main__':
-    from sklearn import datasets
-    iris = datasets.load_iris()
-    X = iris.data[:, :2]  # we only take the first two features.
-    k, g, s = gap(X)
-    print(k)
-    print(g)
-    print(s)
